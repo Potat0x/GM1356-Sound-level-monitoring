354a0911a80ab2d64a813bfc22c121b83f48efff
Potat0x <Potat0x@users.noreply.github.com>
2021-10-09 13:26:21 +0200

    Add "retry" script for running splread infinitely

---
+++ infinity-splread.sh
@@ -0,0 +1,12 @@
+yellow='\033[1;33m'
+no_color='\033[0m' # No Color
+
+while true
+do
+    date >> "$0.log"
+    printf "${yellow}starting splread${no_color}\n"   
+    sudo ./splread -f -r 30-80 -i 150
+    sleep 1
+    printf "\n"
+done
+
