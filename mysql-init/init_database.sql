fed170102c34b7e9ce51999b87da995aa92517b3
Potat0x <Potat0x@users.noreply.github.com>
2021-10-10 20:56:41 +0200

    Add Grafana to docker-compose and read only user

--- mysql-init/init_database.sql
+++ mysql-init/init_database.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE gm1356_monitoring;
-
 USE gm1356_monitoring;
-
 CREATE TABLE readings (_value FLOAT(4,1), _timestamp INT);
+
+
+CREATE USER 'grafana'@'%' IDENTIFIED BY 'grafana';
+GRANT SELECT ON gm1356_monitoring.* TO 'grafana'@'%';
+
+FLUSH PRIVILEGES;
